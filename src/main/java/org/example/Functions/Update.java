37bf32377925a6ed74f3275d2adeb400d6f0f964
Lucas Lima <lukinha-lenta@hotmail.com>
2023-11-13 13:49:33 -0300

    Update/Generator aprimorado

--- src/main/java/org/example/Functions/Update.java
+++ src/main/java/org/example/Functions/Update.java
@@ -1,7 +1,5 @@
 package org.example.Functions;
 
-import org.example.DataAcess;
-
 import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,37 +8,28 @@ import java.sql.SQLException;
 public class Update {
     public boolean update(Connection connection) throws SQLException {
         Get get = new Get(connection);
-        try {
-            String update = "UPDATE ";
-            int idProduct = get.getLastInsertId("product") + 1;
-            int idCategory = get.getLastInsertId("category") + 1;
-            int idMaterial = get.getLastInsertId("material") + 1;
-            int idService = get.getLastInsertId("service") + 1;
-            int idSupplier = get.getLastInsertId("supplier") + 1;
-            int idClient = get.getLastInsertId("client") + 1;
-            int idStreet = get.getLastInsertId("street") + 1;
-            int idNeighborhood = get.getLastInsertId("neighborhood") + 1;
-            PreparedStatement addStatement = connection.prepareStatement(update + "next_id SET product_id = " + idProduct);
-            addStatement.addBatch(update + "next_id SET client_id = " + idClient);
-            addStatement.addBatch(update + "next_id SET category_id = " + idCategory);
-            addStatement.addBatch(update + "next_id SET material_id = " + idMaterial);
-            addStatement.addBatch(update + "next_id SET service_id = " + idService);
-            addStatement.addBatch(update + "next_id SET supplier_id = " + idSupplier);
-            addStatement.addBatch(update + "next_id SET street_id = " + idStreet);
-            addStatement.addBatch(update + "next_id SET neighborhood_id = " + idNeighborhood);
-            addStatement.executeBatch();
-            addStatement.execute();
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    public void waiterDevices(Connection connection) throws SQLException {
         String waiterdevices = JOptionPane.showInputDialog("Quantos dispositivos o cliente irá usar ?");
+        String update = "UPDATE ";
+        int idProduct = get.getLastInsertId("product") + 1;
+        int idCategory = get.getLastInsertId("category") + 1;
+        int idMaterial = get.getLastInsertId("material") + 1;
+        int idService = get.getLastInsertId("service") + 1;
+        int idSupplier = get.getLastInsertId("supplier") + 1;
+        int idClient = get.getLastInsertId("client") + 1;
+        int idStreet = get.getLastInsertId("street") + 1;
+        int idNeighborhood = get.getLastInsertId("neighborhood") + 1;
+        PreparedStatement addStatement = connection.prepareStatement(update + "next_id SET product_id = " + idProduct);
+        addStatement.addBatch(update + "next_id SET client_id = " + idClient);
+        addStatement.addBatch(update + "next_id SET category_id = " + idCategory);
+        addStatement.addBatch(update + "next_id SET material_id = " + idMaterial);
+        addStatement.addBatch(update + "next_id SET service_id = " + idService);
+        addStatement.addBatch(update + "next_id SET supplier_id = " + idSupplier);
+        addStatement.addBatch(update + "next_id SET street_id = " + idStreet);
+        addStatement.addBatch(update + "next_id SET neighborhood_id = " + idNeighborhood);
+        addStatement.addBatch("UPDATE app_config SET waiter_devices = " + waiterdevices);
         System.out.println("Inserindo " + waiterdevices + " Waiter_Devices.......");
-        PreparedStatement waiterDevices = connection.prepareStatement("UPDATE app_config SET waiter_devices = " + waiterdevices);
-        waiterDevices.execute();
+        addStatement.executeBatch();
+        addStatement.execute();
+        return true;
     }
 }
--- src/main/java/org/example/Generetor/Generator.java
+++ src/main/java/org/example/Generetor/Generator.java
@@ -3,10 +3,10 @@ package org.example.Generetor;
 import org.apache.poi.ss.usermodel.*;
 import org.example.Query.*;
 import org.example.DataAcess;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Generator {
@@ -49,7 +49,8 @@ public class Generator {
                     if ((codeCell == null || codeCell.getCellType() == CellType.BLANK)
                             && (nameCell == null || nameCell.getCellType() == CellType.BLANK)) {
                         continue;
-                    } if ((nameCell != null && nameCell.getCellType() != CellType.BLANK) || (priceCell != null || priceCell.getCellType() != CellType.BLANK)) {
+                    }
+                    if ((nameCell != null && nameCell.getCellType() != CellType.BLANK) || (priceCell != null && priceCell.getCellType() != CellType.BLANK)) {
                         createProduct.createProduct(connection, sheet, rowIndex);
                     }
                 }
@@ -57,8 +58,6 @@ public class Generator {
                 System.out.println("Tudo Concluido");
                 break;
             case "client":
-                PreparedStatement preparedStatementVarchar = connection.prepareStatement("alter table client MODIFY column name varchar(300)");
-                preparedStatementVarchar.execute();
                 for (rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
                     Row row = sheet.getRow(rowIndex);
                     if (isRowEmpty(row)) {
@@ -74,8 +73,6 @@ public class Generator {
                 }
                 System.out.println("Tudo Concluido no clientes");
             case "service":
-                preparedStatementVarchar = connection.prepareStatement("alter table service MODIFY column name varchar(300)");
-                preparedStatementVarchar.execute();
                 for (rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
                     Row row = sheet.getRow(rowIndex);
                     if (isRowEmpty(row)) {
@@ -87,10 +84,7 @@ public class Generator {
                         emptyRowCount = 0;
                     }
                     Cell codeCell = row.getCell(0);
-                    Cell nameCell = row.getCell(2);
-                    if ((nameCell != null && nameCell.getCellType() != CellType.BLANK)) {
-                        //criar categoria
-                    } else if (codeCell == null || codeCell.getCellType() == CellType.BLANK) {
+                    if (codeCell == null || codeCell.getCellType() == CellType.BLANK) {
                         CreateService createService = new CreateService();
                         createService.createService(connection, sheet, rowIndex);
                     }
@@ -98,8 +92,6 @@ public class Generator {
                 System.out.println("Service Concluido");
                 break;
             case "supplier":
-                preparedStatementVarchar = connection.prepareStatement("alter table supplier MODIFY column name varchar(300)");
-                preparedStatementVarchar.execute();
                 for (rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
                     Row row = sheet.getRow(rowIndex);
                     if (isRowEmpty(row)) {
@@ -119,32 +111,36 @@ public class Generator {
             case "material":
                 for (rowIndex = 2; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
                     Row row = sheet.getRow(rowIndex);
-                    if (row == null) { continue; }
+                    if (row == null) {
+                        continue;
+                    }
                     if (isRowEmpty(row)) {
                         emptyRowCount++;
-                        if (emptyRowCount >= 3) { break; }
-                    } else { emptyRowCount = 0; }
+                        if (emptyRowCount >= 3) {
+                            break;
+                        }
+                    } else {
+                        emptyRowCount = 0;
+                    }
                     Cell codeCell = row.getCell(0);//Codigo
                     Cell nameCell = row.getCell(1);//name
-                    Cell medidaCell = row.getCell(2);//medida
-                    if ((codeCell != null || codeCell.getCellType() != CellType.BLANK) &&
-                            (nameCell != null || nameCell.getCellType() != CellType.BLANK) &&
-                            (medidaCell == null || medidaCell.getCellType() == CellType.BLANK)) {
-                    } else if ((codeCell == null || codeCell.getCellType() == CellType.BLANK) &&
-                            (nameCell != null || nameCell.getCellType() != CellType.BLANK)) {
+                    if ((codeCell == null || codeCell.getCellType() == CellType.BLANK) && (nameCell != null && nameCell.getCellType() != CellType.BLANK)) {
                         CreateMaterial createMaterial = new CreateMaterial();
                         createMaterial.createMaterial(connection, sheet, rowIndex);
                     }
                     System.out.println("Tudo Concluido");
                 }
-                    break;
+                break;
             default:
         }
         CreateExtra createExtra = new CreateExtra();
         createExtra.createExtra(connection);
     }
+
     private static boolean isRowEmpty(Row row) {
-        if (row == null) {return true;}
+        if (row == null) {
+            return true;
+        }
         // Faz a leitura para o código não percorrer a planilha toda e sim só até 3
         // linhas vazias, acima disso ele para de ler
         for (int cellIndex = row.getFirstCellNum() + 2; cellIndex <= row.getLastCellNum(); cellIndex++) {
