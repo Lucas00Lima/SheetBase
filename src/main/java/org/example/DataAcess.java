85af619cc320af9dd4b415d8b54bf470fd7e9cdc
Lucas00Lima <lukinha-lenta@hotmail.com>
2023-12-06 17:44:49 -0300

    Excluindo Lib voltei a usar o POM

--- src/main/java/org/example/DataAcess.java
+++ src/main/java/org/example/DataAcess.java
@@ -1,6 +1,5 @@
 package org.example;
 
-
 import javax.swing.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,9 +8,9 @@ import java.sql.SQLException;
 public class DataAcess {
     public Connection connectionDB() throws SQLException {
         String username = "root";
-        String storedPassword = JOptionPane.showInputDialog("Insira a senha do usuário <root>");
-        String storedDb = JOptionPane.showInputDialog("Insira o banco de dados");
-        String url = "jdbc:mysql://localhost:3306/" + storedDb;
+        String storedPassword = "@soma+";
+        String bancoCliente = JOptionPane.showInputDialog("Insira o numero do cliente EX: (000) ");
+        String url = "jdbc:mysql://localhost:3306/db" + bancoCliente;
         return DriverManager.getConnection(url, username, storedPassword);
     }
 
