f7367742dc2d989dbea8b5fa20af857e21439bfd
Lucas00Lima <lukinha-lenta@hotmail.com>
2023-12-29 18:47:55 -0300

    CheckBox funcionando

--- src/main/java/org/example/HelloController.java
+++ src/main/java/org/example/HelloController.java
@@ -4,67 +4,66 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import org.example.Generetor.Generator;
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class HelloController {
     public AnchorPane containerMaster;
     public GridPane gridPane;
     public Button buttonSearch;
     public VBox vboxCheck;
     @FXML
-    public CheckBox chekSupplier;
+    public CheckBox checkSupplier;
     @FXML
-    public CheckBox chekMaterial;
-    @FXML
-    private CheckBox chekProduct;
+    private CheckBox checkProduct;
     @FXML
     public CheckBox updateSheet;
     @FXML
     private TextField inputLocal;
+    private String sheet;
     @FXML
     private void initialize() {
-        addSelectionListener(chekProduct);
-        addSelectionListener(chekSupplier);
-        addSelectionListener(chekMaterial);
+        addSelectionListener(checkProduct);
+        addSelectionListener(checkSupplier);
     }
     private void addSelectionListener(CheckBox checkBox) {
         checkBox.selectedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
                 // Desmarca os outros CheckBox
-                if (checkBox != chekProduct) {
-                    chekProduct.setSelected(false);
+                if (checkBox != checkProduct) {
+                    checkProduct.setSelected(false);
                 }
-                if (checkBox != chekSupplier) {
-                    chekSupplier.setSelected(false);
-                }
-                if (checkBox != chekMaterial) {
-                    chekMaterial.setSelected(false);
+                if (checkBox != checkSupplier) {
+                    checkSupplier.setSelected(false);
                 }
             }
         });
-        }
+    }
     @FXML
-    protected void onExecuteButtonClick() {
-        if (chekProduct.isSelected()) {
-            executeLogicForCheckBox("Product");
-        } else if (chekSupplier.isSelected()) {
-            executeLogicForCheckBox("Supplier");
-        } else if (chekMaterial.isSelected()) {
-            executeLogicForCheckBox("Material");
+    protected void onExecuteButtonClick() throws SQLException, IOException {
+        if (checkProduct.isSelected()) {
+            executeLogicForCheckBox("product");
+        } else if (checkSupplier.isSelected()) {
+            executeLogicForCheckBox("supplier");
         } else {
             System.out.println("Nenhum CheckBox selecionado.");
         }
     }
-    private void executeLogicForCheckBox(String checkBoxName) {
-        // Coloque a lógica específica para cada CheckBox aqui
+    private void executeLogicForCheckBox(String checkBoxName) throws SQLException, IOException {
+        DataAcess dataAcess = new DataAcess();
+            System.out.println(sheet);
+            Generator generator = new Generator(dataAcess, checkBoxName, sheet);
+            generator.generetor();
         System.out.println("Função associada ao CheckBox '" + checkBoxName + "' executada!");
     }
     @FXML
     protected void onSearchButtonClick() {
         DataAcess dataAcess = new DataAcess();
-        String sheet = dataAcess.accessSheet();
+        sheet = dataAcess.accessSheet();
         inputLocal.setText(sheet);
         System.out.println("Botão de busca clicado!");
     }
