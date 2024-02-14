5be14284a69542ad783fc0f4a926a38218c406ed
Lucas00Lima <lukinha-lenta@hotmail.com>
2024-02-14 14:16:32 -0300

    Criado função para localizar a coluna

---
+++ src/main/java/com/example/functions/FindColumns.java
@@ -0,0 +1,32 @@
+package com.example.functions;
+
+import org.apache.poi.ss.usermodel.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FindColumns {
+    private final String filePath;
+    public FindColumns (String filePath) {
+        this.filePath = filePath;
+    }
+    public int localizarColunas(String coluna) throws IOException {
+        int index = 0;
+        try (FileInputStream arquivo = new FileInputStream(filePath)) {
+            Workbook workbook = WorkbookFactory.create(arquivo);
+            Sheet sheet = workbook.getSheetAt(0);
+            Row primeiraLinha = sheet.getRow(1);
+            int numeroDeColunas = primeiraLinha.getLastCellNum();
+            for (int i = 0; i < numeroDeColunas; i++) {
+                Cell cell = primeiraLinha.getCell(i);
+                if (cell != null && cell.getCellType() == CellType.STRING) {
+                    String valorCelula = cell.getStringCellValue();
+                    if (valorCelula.contains(coluna)) {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+        }
+        return index;
+    }
+}
