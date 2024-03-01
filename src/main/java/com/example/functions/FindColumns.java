592ba3465343950274071bcd97bfc8350b4353ec
Lucas00Lima <lukinha-lenta@hotmail.com>
2024-03-01 15:03:53 -0300

    Ajuste de coluna do FindColumns

--- src/main/java/com/example/functions/FindColumns.java
+++ src/main/java/com/example/functions/FindColumns.java
@@ -14,7 +14,7 @@ public class FindColumns {
         try (FileInputStream arquivo = new FileInputStream(filePath)) {
             Workbook workbook = WorkbookFactory.create(arquivo);
             Sheet sheet = workbook.getSheetAt(0);
-            Row primeiraLinha = sheet.getRow(1);
+            Row primeiraLinha = sheet.getRow(2);
             int numeroDeColunas = primeiraLinha.getLastCellNum();
             for (int i = 0; i < numeroDeColunas; i++) {
                 Cell cell = primeiraLinha.getCell(i);
