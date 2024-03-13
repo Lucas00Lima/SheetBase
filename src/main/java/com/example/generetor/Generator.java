3d1991c820fb584450374fd8ff02113f9463bd54
Lucas00Lima <lukinha-lenta@hotmail.com>
2024-03-13 15:36:03 -0300

    Insert update

--- src/main/java/com/example/generetor/Generator.java
+++ src/main/java/com/example/generetor/Generator.java
@@ -36,126 +36,125 @@ public class Generator {
                 Connection connection = dataAcess.connectionDB();
                 int rowIndex;
                 int emptyRowCount = 0;
-                if (table == null) {
-                    alert.Alerta("Escolha um checkBox");
-                } else {
-                    switch (table) {
-                        case "product":
-                            LogTex.textInfo("Criando produtos e categoria");
-                            for (rowIndex = 2; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
-                                Row row = sheet.getRow(rowIndex);
-                                if (row == null) {
-                                    continue;
-                                }
-                                if (isRowEmpty(row)) {
-                                    emptyRowCount++;
-                                    if (emptyRowCount >= 3) {
-                                        break;
-                                    }
-                                } else {
-                                    emptyRowCount = 0;
-                                }
-                                Cell codeCell = row.getCell(0);
-                                Cell nameCell = row.getCell(2);
-                                Cell priceCell = row.getCell(4);
-                                if ((codeCell == null || codeCell.getCellType() == CellType.BLANK) && (nameCell == null || nameCell.getCellType() == CellType.BLANK)) {
-                                    continue;
-                                }
-                                if ((nameCell != null && nameCell.getCellType() != CellType.BLANK) || (priceCell != null && priceCell.getCellType() != CellType.BLANK)) {
-                                    createProduct.createProduct(connection, sheet, rowIndex);
-                                }
+                switch (table) {
+                    case "product":
+                        LogTex.textInfo("Criando produtos e categoria");
+                        for (rowIndex = 2; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+                            Row row = sheet.getRow(rowIndex);
+                            if (row == null) {
+                                continue;
                             }
-                            CreateExtra createExtra = new CreateExtra();
-                            createExtra.createExtra(connection);
-                            Update update = new Update();
-                            update.update(connection);
+                            if (isRowEmpty(row)) {
+                                emptyRowCount++;
+                                if (emptyRowCount >= 3) {
+                                    break;
+                                }
+                            } else {
+                                emptyRowCount = 0;
+                            }
+                            Cell codeCell = row.getCell(0);
+                            Cell nameCell = row.getCell(2);
+                            Cell priceCell = row.getCell(4);
+                            if ((codeCell == null || codeCell.getCellType() == CellType.BLANK) && (nameCell == null || nameCell.getCellType() == CellType.BLANK)) {
+                                continue;
+                            }
+                            if ((nameCell != null && nameCell.getCellType() != CellType.BLANK) || (priceCell != null && priceCell.getCellType() != CellType.BLANK)) {
+                                createProduct.createProduct(connection, sheet, rowIndex);
+                            }
+                        }
+                        CreateExtra createExtra = new CreateExtra();
+                        createExtra.createExtra(connection);
+                        Update update = new Update();
+                        update.update(connection);
+                        LogTex.textInfo("Tudo Concluido");
+                        break;
+                    case "client":
+                        for (rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+                            Row row = sheet.getRow(rowIndex);
+                            if (isRowEmpty(row)) {
+                                emptyRowCount++;
+                                if (emptyRowCount >= 3) {
+                                    break;
+                                }
+                            } else {
+                                emptyRowCount = 0;
+                            }
+                            CreateClient createClient = new CreateClient();
+                            createClient.createClient(connection, sheet, rowIndex);
+                        }
+                        LogTex.textInfo("Tudo Concluido no clientes");
+                        break;
+                    case "service":
+                        for (rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+                            Row row = sheet.getRow(rowIndex);
+                            if (isRowEmpty(row)) {
+                                emptyRowCount++;
+                                if (emptyRowCount >= 3) {
+                                    break;
+                                }
+                            } else {
+                                emptyRowCount = 0;
+                            }
+                            Cell codeCell = row.getCell(0);
+                            if (codeCell == null || codeCell.getCellType() == CellType.BLANK) {
+                                CreateService createService = new CreateService();
+                                createService.createService(connection, sheet, rowIndex);
+                            }
+                        }
+                        LogTex.textInfo("Service Concluido");
+                        break;
+                    case "supplier":
+                        for (rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+                            Row row = sheet.getRow(rowIndex);
+                            if (isRowEmpty(row)) {
+                                emptyRowCount++;
+                                if (emptyRowCount >= 3) {
+                                    break;
+                                }
+                            } else {
+                                emptyRowCount = 0;
+                            }
+                            CreateSupplier createSupplier = new CreateSupplier();
+                            createSupplier.createSupplier(connection, sheet, rowIndex);
+                        }
+                        LogTex.textInfo("Tudo Concluido");
+                        break;
+                    case "material":
+                        for (rowIndex = 2; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+                            Row row = sheet.getRow(rowIndex);
+                            if (row == null) {
+                                continue;
+                            }
+                            if (isRowEmpty(row)) {
+                                emptyRowCount++;
+                                if (emptyRowCount >= 3) {
+                                    break;
+                                }
+                            } else {
+                                emptyRowCount = 0;
+                            }
+                            Cell codeCell = row.getCell(0);//Codigo
+                            Cell nameCell = row.getCell(1);//name
+                            if ((codeCell == null || codeCell.getCellType() == CellType.BLANK) && (nameCell != null && nameCell.getCellType() != CellType.BLANK)) {
+                                CreateMaterial createMaterial = new CreateMaterial();
+                                createMaterial.createMaterial(connection, sheet, rowIndex);
+                            }
                             LogTex.textInfo("Tudo Concluido");
-                            break;
-                        case "client":
-                            for (rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
-                                Row row = sheet.getRow(rowIndex);
-                                if (isRowEmpty(row)) {
-                                    emptyRowCount++;
-                                    if (emptyRowCount >= 3) {
-                                        break;
-                                    }
-                                } else {
-                                    emptyRowCount = 0;
-                                }
-                                CreateClient createClient = new CreateClient();
-                                createClient.createClient(connection, sheet, rowIndex);
-                            }
-                            LogTex.textInfo("Tudo Concluido no clientes");
-                            break;
-                        case "service":
-                            for (rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
-                                Row row = sheet.getRow(rowIndex);
-                                if (isRowEmpty(row)) {
-                                    emptyRowCount++;
-                                    if (emptyRowCount >= 3) {
-                                        break;
-                                    }
-                                } else {
-                                    emptyRowCount = 0;
-                                }
-                                Cell codeCell = row.getCell(0);
-                                if (codeCell == null || codeCell.getCellType() == CellType.BLANK) {
-                                    CreateService createService = new CreateService();
-                                    createService.createService(connection, sheet, rowIndex);
-                                }
-                            }
-                            LogTex.textInfo("Service Concluido");
-                            break;
-                        case "supplier":
-                            for (rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
-                                Row row = sheet.getRow(rowIndex);
-                                if (isRowEmpty(row)) {
-                                    emptyRowCount++;
-                                    if (emptyRowCount >= 3) {
-                                        break;
-                                    }
-                                } else {
-                                    emptyRowCount = 0;
-                                }
-                                CreateSupplier createSupplier = new CreateSupplier();
-                                createSupplier.createSupplier(connection, sheet, rowIndex);
-                            }
-                            LogTex.textInfo("Tudo Concluido");
-                            break;
-                        case "material":
-                            for (rowIndex = 2; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
-                                Row row = sheet.getRow(rowIndex);
-                                if (row == null) {
-                                    continue;
-                                }
-                                if (isRowEmpty(row)) {
-                                    emptyRowCount++;
-                                    if (emptyRowCount >= 3) {
-                                        break;
-                                    }
-                                } else {
-                                    emptyRowCount = 0;
-                                }
-                                Cell codeCell = row.getCell(0);//Codigo
-                                Cell nameCell = row.getCell(1);//name
-                                if ((codeCell == null || codeCell.getCellType() == CellType.BLANK) && (nameCell != null && nameCell.getCellType() != CellType.BLANK)) {
-                                    CreateMaterial createMaterial = new CreateMaterial();
-                                    createMaterial.createMaterial(connection, sheet, rowIndex);
-                                }
-                                LogTex.textInfo("Tudo Concluido");
-                            }
-                            break;
-                        default:
-                    }
+                        }
+                        break;
+                    default:
                 }
             }
+//            }
         } catch (SQLException | IOException e) {
-            LogTex.textError("Erro no generator");
+            throw new RuntimeException("Erro no generator", e);
         }
     }
 
     private static boolean isRowEmpty(Row row) {
-        if (row == null) {return true;}
+        if (row == null) {
+            return true;
+        }
         // Faz a leitura para o código não percorrer a planilha toda e sim só até 3
         // linhas vazias, acima disso ele para de ler
         for (int cellIndex = row.getFirstCellNum() + 2; cellIndex <= row.getLastCellNum(); cellIndex++) {
--- src/main/java/com/example/generetor/GeneratorUpdate.java
+++ src/main/java/com/example/generetor/GeneratorUpdate.java
@@ -8,7 +8,6 @@ import org.apache.poi.ss.usermodel.*;
 import java.io.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import static com.example.TreatmentRows.dataFormatter;
 
 //TODO: Esta classe precisa ser revisada e otimizada
@@ -22,70 +21,82 @@ public class GeneratorUpdate {
         this.filePath = filePath;
     }
 
-    public void UpdateProduct() throws SQLException, IOException {
-        Connection connection = dataAcess.connectionDB();
-        FileInputStream fileInput = new FileInputStream(filePath);
-        Workbook workbook = WorkbookFactory.create(fileInput);
-        Sheet sheet = workbook.getSheetAt(0);
-        FindColumns findColumns = new FindColumns(filePath);
-        int rowIndex;
-        int emptyRowCount = 0;
-        for (rowIndex = 3; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
-            Row row = sheet.getRow(rowIndex);
-            if (row == null) {continue;}
-            if (isRowEmpty(row)) {
-                emptyRowCount++;
-                if (emptyRowCount >= 3) {break;}
-            } else {emptyRowCount = 0;}
-            String id = dataFormatter.formatCellValue(row.getCell(0));
-            String ncm = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("NCM")));
-            String cfop = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("CFOP")));
-            String cest = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("CEST")));
-            String cst = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("CST")));
-            if (cst.length() == 1) {cst = cst + "0";}
-            String icmsString = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("ICMS Aliq")));
-            icmsString = icmsString.replace(",", "");
-            icmsString = icmsString + "0" + "0" + "0";
-            int icms = Integer.parseInt(icmsString);
+    public void UpdateProduct() {
+        try {
+            Connection connection = dataAcess.connectionDB();
+            FileInputStream fileInput = new FileInputStream(filePath);
+            Workbook workbook = WorkbookFactory.create(fileInput);
+            Sheet sheet = workbook.getSheetAt(0);
+            FindColumns findColumns = new FindColumns(filePath);
+            int rowIndex;
+            int emptyRowCount = 0;
+            for (rowIndex = 3; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+                Row row = sheet.getRow(rowIndex);
+                if (row == null) {
+                    continue;
+                }
+                if (isRowEmpty(row)) {
+                    emptyRowCount++;
+                    if (emptyRowCount >= 3) {
+                        break;
+                    }
+                } else {
+                    emptyRowCount = 0;
+                }
+                String id = dataFormatter.formatCellValue(row.getCell(0));
+                String ncm = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("NCM")));
+                String cfop = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("CFOP")));
+                String cest = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("CEST")));
+                String cst = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("CST")));
+                if (cst.length() == 1) {
+                    cst = cst + "0";
+                }
+                String icmsString = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("ICMS Aliq")));
+                icmsString = icmsString.replace(",", "");
+                icmsString = icmsString + "0" + "0" + "0";
+                int icms = Integer.parseInt(icmsString);
 
-            String pisCod = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("PIS Cod")));
-            String pisAString = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("PIS Aliq")));
-            pisAString = pisAString.replace(",", "");
-            pisAString = pisAString + "0";
-            int pisA = Integer.parseInt(pisAString);
+                String pisCod = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("PIS Cod")));
+                String pisAString = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("PIS Aliq")));
+                pisAString = pisAString.replace(",", "");
+                pisAString = pisAString + "0";
+                int pisA = Integer.parseInt(pisAString);
 
-            String cofinsCod = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("COFINS Cod")));
-            cofinsCod = "0" + cofinsCod;
-            String cofinsAString = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("COFINS Aliq")));
-            cofinsAString = cofinsAString.replace(",", "");
-            cofinsAString = cofinsAString + "0" + "0" + "0";
-            int cofinsA = Integer.parseInt(cofinsAString);
+                String cofinsCod = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("COFINS Cod")));
+                cofinsCod = "0" + cofinsCod;
+                String cofinsAString = dataFormatter.formatCellValue(row.getCell(findColumns.localizarColunas("COFINS Aliq")));
+                cofinsAString = cofinsAString.replace(",", "");
+                cofinsAString = cofinsAString + "0" + "0" + "0";
+                int cofinsA = Integer.parseInt(cofinsAString);
 
-            String sql = "UPDATE product " +
-                    "SET ncm = ?, " +
-                    "cfop = ?, " +
-                    "tax4_code = ?, " +
-                    "tax1_code = ?, " +
-                    "tax1 = ?, " +
-                    "tax2_code = ?, " +
-                    "tax2 = ?, " +
-                    "tax3_code = ?, " +
-                    "tax3 = ? " +
-                    "WHERE internal_code = ?";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, ncm);
-            preparedStatement.setString(2, cfop);
-            preparedStatement.setString(3, cest);
-            preparedStatement.setString(4, cst);
-            preparedStatement.setInt(5, icms);
-            preparedStatement.setString(6, pisCod);
-            preparedStatement.setInt(7, pisA);
-            preparedStatement.setString(8, cofinsCod);
-            preparedStatement.setInt(9, cofinsA);
-            preparedStatement.setString(10, id);
-            preparedStatement.executeUpdate();
+                String sql = "UPDATE product " +
+                        "SET ncm = ?, " +
+                        "cfop = ?, " +
+                        "tax4_code = ?, " +
+                        "tax1_code = ?, " +
+                        "tax1 = ?, " +
+                        "tax2_code = ?, " +
+                        "tax2 = ?, " +
+                        "tax3_code = ?, " +
+                        "tax3 = ? " +
+                        "WHERE internal_code = ?";
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                preparedStatement.setString(1, ncm);
+                preparedStatement.setString(2, cfop);
+                preparedStatement.setString(3, cest);
+                preparedStatement.setString(4, cst);
+                preparedStatement.setInt(5, icms);
+                preparedStatement.setString(6, pisCod);
+                preparedStatement.setInt(7, pisA);
+                preparedStatement.setString(8, cofinsCod);
+                preparedStatement.setInt(9, cofinsA);
+                preparedStatement.setString(10, id);
+                preparedStatement.executeUpdate();
+            }
+            LogTex.textInfo("Atualização Concluida");
+        } catch (Exception e) {
+            LogTex.textError("Erro na atualização " + e);
         }
-        LogTex.textInfo("Atualização Concluida");
     }
     private static boolean isRowEmpty(Row row) {
         if (row == null) {return true;}
