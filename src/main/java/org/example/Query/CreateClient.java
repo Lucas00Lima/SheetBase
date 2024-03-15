2316fbf6d83cde7f686bfca4c262b6d643ad37bf
Lucas00Lima <lukinha-lenta@hotmail.com>
2024-03-15 19:46:34 -0300

    Função para tratar do numero

--- src/main/java/org/example/Query/CreateClient.java
+++ src/main/java/org/example/Query/CreateClient.java
@@ -4,7 +4,6 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.example.Functions.InsertQuery;
 import org.example.entidades.Client;
 import org.example.factory.ClienteFactory;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -12,45 +11,59 @@ import java.sql.SQLException;
 public class CreateClient {
     public void createClient(Connection connection, Sheet sheet, int rowIndex) throws SQLException {
         String table = "client";
-        ClienteFactory cliente = new ClienteFactory();
-        Client clientData = cliente.cliente(sheet, rowIndex, connection);
+        Client client = new Client();
+        ClienteFactory clientes = new ClienteFactory();
+        clientes.cliente(sheet, rowIndex,connection, client);
         InsertQuery insertQuery = new InsertQuery();
         String insertQueryClient = insertQuery.insert(table, connection);
         PreparedStatement preparedStatement = connection.prepareStatement(insertQueryClient);
-        preparedStatement.setString(1, clientData.getId());
-        preparedStatement.setString(2, clientData.getName());
-        preparedStatement.setString(3, clientData.getCompanyName());
+        preparedStatement.setString(1, client.getId());
+        preparedStatement.setString(2, client.getName());
+        preparedStatement.setString(3, client.getCompanyName());
         preparedStatement.setString(4, "");
-        preparedStatement.setInt(5, clientData.getType1()); // CPF ou CNPJ
+        preparedStatement.setInt(5, client.getType1()); // CPF ou CNPJ
         preparedStatement.setInt(6, 1); // type2 ??
-        preparedStatement.setInt(7, clientData.getGender()); // M ou F
-        preparedStatement.setDate(8, clientData.getBirthday());
+        preparedStatement.setInt(7, client.getGender()); // M ou F
+        preparedStatement.setDate(8, client.getBirthday());
         preparedStatement.setString(9, "");
-        preparedStatement.setString(10, clientData.getTel());
-        preparedStatement.setString(11, clientData.getCelContact1());
-        preparedStatement.setString(12, clientData.getNameContact1());
+        if (client.getTel() == null && client.getCellPhone() == null) {
+            preparedStatement.setString(10, "");
+            preparedStatement.setString(11, "");
+        } else {
+            if (client.getTel() == null) {
+                preparedStatement.setString(10, "");
+            } else {
+                preparedStatement.setString(10, client.getTel());
+            }
+            if (client.getCellPhone() == null) {
+                preparedStatement.setString(11, "");
+            } else {
+                preparedStatement.setString(11, client.getCellPhone());
+            }
+        }
+        preparedStatement.setString(12, client.getNameContact1());
         preparedStatement.setString(13, ""); // phone2
-        preparedStatement.setString(14, clientData.getCelContact2());
-        preparedStatement.setString(15, clientData.getNameContact2());
-        preparedStatement.setString(16, clientData.getEmail());
+        preparedStatement.setString(14, client.getCelContact2());
+        preparedStatement.setString(15, client.getNameContact2());
+        preparedStatement.setString(16, client.getEmail());
         preparedStatement.setInt(17, 0);
-        preparedStatement.setInt(18, clientData.getState());
-        preparedStatement.setInt(19, clientData.getCity());
-        preparedStatement.setString(20, clientData.getStreet());
+        preparedStatement.setInt(18, client.getState());
+        preparedStatement.setInt(19, client.getCity());
+        preparedStatement.setString(20, client.getStreet());
         preparedStatement.setString(21, "");
         preparedStatement.setString(22, "");
         preparedStatement.setInt(23, 0); // neighborhood
         preparedStatement.setString(24, "");
         preparedStatement.setString(25, "");
 
-        if (clientData.getType1() == 1) {
-            preparedStatement.setString(26, clientData.getNumDoc()); // doc_number2 = CPF
+        if (client.getType1() == 1) {
+            preparedStatement.setString(26, client.getNumDoc()); // doc_number2 = CPF
             preparedStatement.setString(27, ""); // doc_number3 = CNPJ
             preparedStatement.setString(28, ""); // doc_number4 = IE
-        } else if (clientData.getType1() == 2) {
+        } else if (client.getType1() == 2) {
             preparedStatement.setString(26, ""); // doc_number2 = CPF
-            preparedStatement.setString(27, clientData.getNumDoc()); // doc_number3 = CNPJ
-            preparedStatement.setString(28, clientData.getNumDoc2()); // doc_number4 = IE
+            preparedStatement.setString(27, client.getNumDoc()); // doc_number3 = CNPJ
+            preparedStatement.setString(28, client.getNumDoc2()); // doc_number4 = IE
         }
         preparedStatement.setString(29, "");
         preparedStatement.setInt(30, 0);
@@ -62,7 +75,7 @@ public class CreateClient {
         preparedStatement.setInt(36, 0); // client_credit_limit
         preparedStatement.setInt(37, 0);
         preparedStatement.setInt(38, 0);
-        preparedStatement.setDate(39, clientData.getRegister()); // register
+        preparedStatement.setDate(39, client.getRegister()); // register
         preparedStatement.setString(40, "");
         preparedStatement.setInt(41, 0);
         preparedStatement.setInt(42, 1);
@@ -74,6 +87,5 @@ public class CreateClient {
         preparedStatement.setString(48, "");
         preparedStatement.setInt(49, 0);
         preparedStatement.execute();
-        System.out.println(preparedStatement);
     }
 }
