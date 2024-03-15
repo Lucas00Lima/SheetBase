fd0280a0a0b35f0bd6407f52c78578c1791f73eb
Lucas00Lima <lukinha-lenta@hotmail.com>
2024-03-15 19:46:05 -0300

    Transformei em loombok o client

--- src/main/java/org/example/factory/ClienteFactory.java
+++ src/main/java/org/example/factory/ClienteFactory.java
@@ -3,19 +3,17 @@ package org.example.factory;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.example.DataAcess;
 import org.example.Functions.Get;
 import org.example.TreatmentRows;
 import org.example.entidades.Client;
-
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class ClienteFactory implements TreatmentRows {
     LocalDate currentDate = LocalDate.now();
-    public Client cliente(Sheet sheet, int rowIndex, Connection connection) throws SQLException {
+
+    public void cliente(Sheet sheet, int rowIndex, Connection connection, Client client) throws SQLException {
         Get get = new Get(connection);
         Row row = sheet.getRow(rowIndex);
         Cell idCell = row.getCell(0);
@@ -35,26 +33,25 @@ public class ClienteFactory implements TreatmentRows {
         Cell nameContact2Cell = row.getCell(14);
         Cell celContact2Cell = row.getCell(15);
         Cell obsCell = row.getCell(16);
-        String id = dataFormatter.formatCellValue(idCell);
-        String name = dataFormatter.formatCellValue(nameCell);
-        int type1 = treatmentVariable.cpfOrCnpj(numDocCell);
-        String numDoc = treatmentVariable.document(numDocCell);
-        String numDoc2 = treatmentVariable.document(numDoc2Cell);
-        String companyName = dataFormatter.formatCellValue(companyNameCell);
-        String tel = dataFormatter.formatCellValue(telCell);
-        int gender = treatmentVariable.gender(genderCell);
-        String email = dataFormatter.formatCellValue(emailCell);
-        Date birthday = treatmentVariable.dateBrithday(birthdayCell);
-        int state = get.stateId(stateCell);
-        int city = get.cityId(cityCell);
-        String street = dataFormatter.formatCellValue(streetCell);
-        String nameContact1 = dataFormatter.formatCellValue(nameContact1Cell);
-        String celContact1 = dataFormatter.formatCellValue(celContact1Cell);
-        String nameContact2 = dataFormatter.formatCellValue(nameContact2Cell);
-        String celContact2 = dataFormatter.formatCellValue(celContact2Cell);
-        String obs = dataFormatter.formatCellValue(obsCell);
-        Date register = java.sql.Date.valueOf(currentDate);
-        Client client = new Client(id,name,numDoc,type1,numDoc2,companyName,tel,gender,email,birthday,state,city,street,nameContact1,celContact1,nameContact2,celContact2,obs,register);
-        return client;
+
+        client.setId(dataFormatter.formatCellValue(idCell));
+        client.setName(dataFormatter.formatCellValue(nameCell));
+        client.setType1(treatmentVariable.cpfOrCnpj(numDocCell));
+        client.setNumDoc(treatmentVariable.document(numDocCell));
+        client.setNumDoc2(treatmentVariable.document(numDoc2Cell));
+        client.setCompanyName(dataFormatter.formatCellValue(companyNameCell));
+        treatmentVariable.verifyCell(client,telCell);
+        client.setGender(treatmentVariable.gender(genderCell));
+        client.setEmail(dataFormatter.formatCellValue(emailCell));
+        client.setBirthday(treatmentVariable.dateBrithday(birthdayCell));
+        client.setState(get.stateId(stateCell));
+        client.setCity(get.cityId(cityCell));
+        client.setStreet(dataFormatter.formatCellValue(streetCell));
+        client.setNameContact1(dataFormatter.formatCellValue(nameContact1Cell));
+        client.setCelContact1(dataFormatter.formatCellValue(celContact1Cell));
+        client.setNameContact2(dataFormatter.formatCellValue(nameContact2Cell));
+        client.setCelContact2(dataFormatter.formatCellValue(celContact2Cell));
+        client.setObs(dataFormatter.formatCellValue(obsCell));
+        client.setRegister(java.sql.Date.valueOf(currentDate));
     }
 }
