8d4809ffc2e94db73b22147508ba3b359a0e3a9c
Lucas00Lima <lukinha-lenta@hotmail.com>
2024-03-25 10:57:36 -0300

    Verificando primeiroCaracter = 0

--- src/main/java/org/example/Functions/TreatmentVariable.java
+++ src/main/java/org/example/Functions/TreatmentVariable.java
@@ -154,51 +154,45 @@ public class TreatmentVariable {
     public void verifyCell(Client client, Cell celPhone) {
         String fone = dataFormatter.formatCellValue(celPhone);
         if (!fone.isEmpty()) {
-            if (fone.contains("(") || fone.contains(")")) {
-                fone = fone.replace("(", "").replace(")", "");
-            }
-            if (fone.contains(" ")) {
-                fone = fone.replace(" ", "");
-            }
-            if (fone.contains("-")) {
-                fone = fone.replace("-", "");
-            }
+            fone = fone.replace("(", "").replace(")", "").replace(" ", "").replace("-", "");
             StringBuilder stringBuilder = new StringBuilder(fone);
             char primeiroCaractere;
             primeiroCaractere = stringBuilder.charAt(0);
-            if (fone.length() == 8) {
-                if ((primeiroCaractere == '9') || (primeiroCaractere == '3')) {
-                    stringBuilder.insert(0, "(");
-                    stringBuilder.insert(1, "1");
-                    stringBuilder.insert(2, "4");
-                    stringBuilder.insert(3, ")");
-                    stringBuilder.insert(8, "-");
-                    System.out.println(stringBuilder);
-                    client.setTel(String.valueOf(stringBuilder));
-                    client.setCellPhone("");
-                } else if (primeiroCaractere == '1') {
-                    stringBuilder.insert(0, "(");
-                    stringBuilder.insert(3, ")");
-                    stringBuilder.insert(8, "-");
-                    System.out.println(stringBuilder);
-                    client.setTel(String.valueOf(stringBuilder));
-                    client.setCellPhone("");
-                } else {
-                    System.out.println(stringBuilder);
-                    client.setTel(String.valueOf(stringBuilder));
-                    client.setCellPhone("");
-                }
+            if (primeiroCaractere == '0') {
+                stringBuilder.deleteCharAt(0);
             }
-            if (fone.length() == 9) {
-                if (primeiroCaractere == '9') {
+            if (primeiroCaractere == '9' || primeiroCaractere == '3') {
+                if (fone.length() == 8) {
                     stringBuilder.insert(0, "(");
                     stringBuilder.insert(1, "1");
                     stringBuilder.insert(2, "4");
                     stringBuilder.insert(3, ")");
-                    stringBuilder.insert(9, "-");
+                    stringBuilder.insert(8, "-");
                     System.out.println(stringBuilder);
-                    client.setTel("");
-                    client.setCellPhone(String.valueOf(stringBuilder));
+                    client.setTel(String.valueOf(stringBuilder));
+                    client.setCellPhone("");
+                } else if (fone.length() >= 9) {
+                    if (primeiroCaractere == '9') {
+                        stringBuilder.insert(0, "(");
+                        stringBuilder.insert(1, "1");
+                        stringBuilder.insert(2, "4");
+                        stringBuilder.insert(3, ")");
+                        stringBuilder.insert(9, "-");
+                        System.out.println(stringBuilder);
+                        client.setTel("");
+                        client.setCellPhone(String.valueOf(stringBuilder));
+                    } else if (primeiroCaractere == '1') {
+                        stringBuilder.insert(0, "(");
+                        stringBuilder.insert(3, ")");
+                        stringBuilder.insert(8, "-");
+                        System.out.println(stringBuilder);
+                        client.setTel(String.valueOf(stringBuilder));
+                        client.setCellPhone("");
+                    } else {
+                        System.out.println(stringBuilder);
+                        client.setTel(String.valueOf(stringBuilder));
+                        client.setCellPhone("");
+                    }
                 } else if (primeiroCaractere == '1') {
                     stringBuilder.insert(0, "(");
                     stringBuilder.insert(3, ")");
@@ -211,13 +205,6 @@ public class TreatmentVariable {
                     client.setTel("");
                     client.setCellPhone(String.valueOf(stringBuilder));
                 }
-            } else {
-                stringBuilder.insert(0, "(");
-                stringBuilder.insert(3, ")");
-                stringBuilder.insert(10, "-");
-                System.out.println(stringBuilder);
-                client.setTel("");
-                client.setCellPhone(String.valueOf(stringBuilder));
             }
         }
     }
